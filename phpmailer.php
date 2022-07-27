939cd95c7d1c9aedf2ae2b96365368913a7764dc
Shubham Sharma <Shubham49412@gmail.com>
2022-07-27 15:39:27 +0530

    first commit

--- phpmailer.php
+++ phpmailer.php
@@ -26,15 +26,15 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'testphpmail.shubham@gmail.com';                     //SMTP username
-    $mail->Password   = 'yvlvuilyhdlkznbh';                               //SMTP password
+    $mail->Username   = '';                     //SMTP username
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = 'tls';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('testphpmail.shubham@gmail.com');
+    $mail->setFrom('');
 
-    $mail->addAddress('testphpmail.shubham@gmail.com');     //Add a recipient
+    $mail->addAddress('');     //Add a recipient
 
 
     //Content
@@ -57,13 +57,13 @@ try {
     $newMail->isSMTP();                                            //Send using SMTP
     $newMail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $newMail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $newMail->Username   = 'testphpmail.shubham@gmail.com';                     //SMTP username
-    $newMail->Password   = 'yvlvuilyhdlkznbh';                               //SMTP password
+    $newMail->Username   = '';                     //SMTP username
+    $newMail->Password   = '';                               //SMTP password
     $newMail->SMTPSecure = 'tls';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $newMail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $newMail->setFrom('testphpmail.shubham@gmail.com');
+    $newMail->setFrom('');
 
     $newMail->addAddress($userEmail);     //Add a recipient
 $subj= 'Thank you for visiting Drivaar';
